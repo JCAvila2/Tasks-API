ff590f70c0f2243eef194189f6b6df6b51fb1be8
JCAvila2 <juankmilo1102@hotmail.com>
2024-02-28 11:27:43 -0500

    DB credentials added to .env file

--- src/db.js
+++ src/db.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const myqls = require('mysql');
 
 const connection = myqls.createConnection({
-    host: 'localhost',
-    user: 'api',
-    password: '1234',
-    database: 'tasksapi'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 connection.connect((error) => {
